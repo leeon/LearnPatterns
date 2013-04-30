d1a51cd8108d0280c705d69f11086dafb09f8f03
leeon <leeon.ly@foxmail.com>
2013-05-01 01:00:24 +0800

    init the python branch

--- src/org/leeon/pattern/decorator/Decorator.java
+++
@@ -1,9 +0,0 @@
-package org.leeon.pattern.decorator;
-/**
- * To make sure that decorators and the decoratored are from the same root
- * @author leeon
- * 2013.4.30
- * */
-public abstract class Decorator extends Component{
-   //TODO YOU may write something else
-}
